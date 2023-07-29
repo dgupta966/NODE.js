cec4038ee2679e3cc75dd1fe404b8b0fc91039e2
Deepak Gupta <dgupta966@gmail.com>
2023-07-30 00:24:14 +0530

    async write file and create routes - import/export

--- app.js
+++ app.js
@@ -1,45 +1,8 @@
 const http = require("http");
-const fs = require("fs");
+const routes = require("./routes");
 
-const server = http.createServer((req, res) => {
-  //   console.log(req.url, req.method, req.headers);
-  //process.exit();
+console.log(routes.text);
 
-  const url = req.url;
-  const method = req.method;
-  if (url === "/") {
-    res.write("<html>");
-    res.write("<head><title>form</title></head>");
-    res.write(
-      "<body><form action='/message' method='POST'><input type='text' name='message'><button type='submit'>send</button></form></body>"
-    );
-    res.write("</html>");
-    return res.end();
-  }
-  if (url === "/message" && method === "POST") {
-    const body = [];
-    req.on("data", (chunk) => {
-      console.log(chunk);
-      body.push(chunk);
-    }); // data event will be fired when new chunk ready to read
-    req.on("end", () => {
-      const parseBody = Buffer.concat(body).toString();
-      //console.log(parseBody);
-      const message = parseBody.split("=")[1];
-
-      fs.writeFileSync("message.txt", message);
-    });
-    res.statusCode = 302;
-    res.setHeader("Location", "/");
-    return res.end();
-  }
-
-  res.setHeader("Content-Type", "text/html");
-  res.write("<html>");
-  res.write("<head><title>My first page</title></head>");
-  res.write("<body><h1>Hello from node.js server</h1></body>");
-  res.write("</html>");
-  res.end();
-});
+const server = http.createServer(routes.handler);
 
 server.listen(3000);
