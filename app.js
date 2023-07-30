6be347ade3146d70b1c4b530f7cc55494a30d502
Deepak Gupta <dgupta966@gmail.com>
2023-07-30 11:54:35 +0530

    initial -> express - setup

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
-const http = require("http");
-const routes = require("./routes");
+// const http = require("http");
 
-console.log(routes.text);
+const express = require("express");
 
-const server = http.createServer(routes.handler);
+const app = express();
 
-server.listen(3000);
+app.use((req, res, next) => {
+  console.log("In the middleware");
+  next(); // next allows request to continue on next middleware
+}); // 'use' allows us to add middleware functions
+
+app.use((req, res, next) => {
+  console.log("In the another middleware");
+  //   res.setHeader(); we can override this also
+  res.send("<h1>Hello from express!</h1>");
+  // https://github.com/expressjs/express/blob/master/lib/response.js
+});
+
+// const server = http.createServer(app);
+
+// server.listen(3000);
+
+app.listen(3000); // https://github.com/expressjs/express/blob/master/lib/application.js
