9cfd21d2c9a32e17ae8e5bc83975acfc119efdf7
Deepak Gupta <dgupta966@gmail.com>
2023-07-31 10:23:46 +0530

    body-parser -> parsing incoming request

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 
 const express = require("express");
 
+const bodyParser = require("body-parser");
+
 const app = express();
 /*
 app.use((req, res, next) => {
@@ -11,18 +13,27 @@ app.use((req, res, next) => {
 
 */ // https://expressjs.com/en/4x/api.html#app.use
 
-app.use("/", (req, res, next) => {
-  console.log("this always runs");
-  next();
-});
+// app.use("/", (req, res, next) => {
+//   // console.log("this always runs");
+//   next();
+// });
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/add-product", (req, res, next) => {
-  console.log("In the Add product page middleware");
-  res.send("<h1>Add product page!</h1>");
+  // console.log("In the Add product page middleware");
+  res.send(
+    "<form action='/products' method='POST'><input type='text' name='title' /><button type='submit'>Add products</button></form>"
+  );
+});
+
+app.use("/products", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 
 app.use("/", (req, res, next) => {
-  console.log("In the another middleware");
+  // console.log("In the another middleware");
   //   res.setHeader(); we can override this also
   res.send("<h1>Hello from express!</h1>");
   // https://github.com/expressjs/express/blob/master/lib/response.js
