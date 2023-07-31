b6148419132f6a47a4b0b9d6423f2c18535208c4
Deepak Gupta <dgupta966@gmail.com>
2023-07-31 11:42:11 +0530

    add page not found in HTML

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 // const http = require("http");
 
+const path = require("path");
+
 const express = require("express");
 
 const adminRoutes = require("./routes/admin");
@@ -28,7 +30,11 @@ app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).send("<h1>Page not found !</h1>");
+  // res.status(404).send("<h1>Page not found !</h1>");
+
+  res
+    .status(404)
+    .sendFile(path.join(__dirname, "views", "page-not-found.html"));
 });
 
 // const server = http.createServer(app);
