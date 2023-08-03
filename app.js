2b4fcf3f45e954f1da9bc2ff9c574801c42a1755
Deepak Gupta <dgupta966@gmail.com>
2023-08-03 16:16:40 +0530

    serving files statically

--- app.js
+++ app.js
@@ -25,6 +25,8 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use(express.static(path.join(__dirname, "public")));
+
 app.use("/admin", adminRoutes);
 
 app.use(shopRoutes);
